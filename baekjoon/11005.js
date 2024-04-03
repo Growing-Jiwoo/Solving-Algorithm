e379f58f76f990c511ae3d7bb1d78074598f5985
Growing-Jiwoo <fbaa4412@naver.com>
2024-04-03 22:03:33 +0900

    문자열 : 진법 변환 2

---
+++ baekjoon/11005.js
@@ -0,0 +1,19 @@
+let [N, M] = require("fs")
+  .readFileSync("11005.txt")
+  .toString()
+  .trim()
+  .split(" ");
+
+let result = "";
+
+while (N > 0) {
+  let remainder = N % M;
+  if (remainder < 10) {
+    result = remainder + result;
+  } else {
+    result = String.fromCharCode(remainder + 55) + result;
+  }
+  N = Math.floor(N / M);
+}
+
+console.log(result);
